6dc53a5970ce3cd4c8e96b4b0ddb56cd667b1995
Britt Gresham <britt@brittg.com>
2024-12-14 13:25:29 -0800

    Update to use flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Build environment
+              gnumake42
+              go
+
+              # Backend
+              buf
+              grpcurl
+
+              # Frontend
+              nodejs_20
+              nodePackages.npm
+            ];
+          };
+        }
+      );
+}
